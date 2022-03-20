77f339b91952676dbc5a4048e2f29827643fa78c
Aniket Rai <prosniperaniket@gmail.com>
2022-03-20 16:19:30 +0530

    Auth0 added

--- code/pages/_app.js
+++ code/pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
