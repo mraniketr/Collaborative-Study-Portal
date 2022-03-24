371b68e8f460f510b7e7fbe28bc35eba45314d94
Amit Nemade <amitnemade34@gmail.com>
2022-03-24 23:55:29 +0530

    form page added with api

---
+++ code/pages/courses/[courseId]/[subjectId]/[chapterId]/[topicId]/update.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import * as yup from "yup";
+import { Field, Form, Formik, useFormik } from "formik";
+import { useRouter } from "next/router";
+import Navbar from "../../../../../../components/Navbar";
+
+const update = () => {
+	const router = useRouter();
+	const { courseId, subjectId, chapterId, topicId } = router.query;
+	const [initialValue, setInitialValue] = useState({
+		topicName: "Light1 New 2",
+		topicDescription: "lorem ipsum sadasdas asdasdasdasda",
+		videoURL:
+			"https://www.youtube.com/watch?v=Xf_VZ8GxU1Y&ab_channel=Don%27tMemorise",
+	});
+
+	const validationSchema = yup.object().shape({
+		topicName: yup.string().required(),
+		topicDescription: yup.string().required(),
+		videoURL: yup.string().required(),
+	});
+	return (
+		<React.Fragment>
+			<Navbar />
+			<div className="flex flex-col p-16">
+				<Formik
+					initialValues={initialValue}
+					validationSchema={() => validationSchema}
+					onSubmit={async (values) => {
+						alert(JSON.stringify(values, null, 2));
+						var res = await fetch("http://localhost:3000/api/InsertData", {
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({
+								collection: "topics",
+								insertObj: {
+									activeStatus: false,
+									topicId: topicId,
+									topicName: values.topicName,
+									topicDescription: values.topicDescription,
+									videoURL: values.videoURL,
+									chapterId: chapterId,
+									courseId: courseId,
+									subjectId: subjectId,
+								},
+							}),
+						});
+						const data = await res.json();
+						console.log(data);
+						router.push(
+							`/courses/${courseId}/${subjectId}/${chapterId}/${topicId}/learn`
+						);
+					}}
+				>
+					<Form className="flex flex-row justify-center">
+						<div className="flex flex-col w-[500px] mt-4 bg-white rounded-lg p-6">
+							<div className="flex flex-row mb-3 text-2xl">Edit Topic</div>
+							<div className="flex flex-col my-3">
+								<label htmlFor="firstName" className="text-sm">
+									Topic Name
+								</label>
+								<Field
+									id="topicName"
+									name="topicName"
+									className="px-3 py-2 border border-gray-200 rounded-md"
+									placeholder="Jane"
+								/>
+							</div>
+							<div className="flex flex-col my-3">
+								<label htmlFor="lastName" className="text-sm">
+									Topic Description
+								</label>
+								<Field
+									id="topicDescription"
+									name="topicDescription"
+									className="px-3 py-2 border border-gray-200 rounded-md"
+									placeholder="Write description here..."
+								/>
+							</div>
+							<div className="flex flex-col my-3">
+								<label htmlFor="lastName" className="text-sm">
+									Video URL
+								</label>
+								<Field
+									id="videoURL"
+									name="videoURL"
+									className="px-3 py-2 border border-gray-200 rounded-md focus:border focus:outline-1"
+									placeholder="https://www.youtube.com/watch?v=zueyEdRZQlk"
+								/>
+							</div>
+							<button
+								type="submit"
+								className="px-5 py-2 mt-1 text-blue-700 border border-blue-700 rounded-md hover:text-white hover:bg-blue-700 w-fit"
+							>
+								Submit
+							</button>
+						</div>
+					</Form>
+				</Formik>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default update;
