ccbdb22306f867bbaaf3e9791cc104bf74346d0f
Bhagyesh <56735326+bhagyeshrane@users.noreply.github.com>
2022-03-26 01:33:37 +0530

    add react components
    added accordion and carousel. styling remaining

--- code/pages/index.js
+++ code/pages/index.js
@@ -3,6 +3,15 @@ import Image from "next/image";
 import { useUser } from "@auth0/nextjs-auth0";
 import Navbar from "../components/Navbar";
 
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Collapsible from 'react-collapsible';
+import { Button } from 'react-responsive-button';
+import 'react-responsive-button/dist/index.css';
+
+
+
 export default function Index() {
   const { user, error, isLoading } = useUser();
 
@@ -11,25 +20,76 @@ export default function Index() {
 
   if (user) {
     return (
+  <div>
       <div>
-        <Head>
-          <title>Collaborative Study Portal</title>
-          <meta name="description" content="Learn together Grow together" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
         <Navbar />
+      </div>
+    <div>
+      <Button href="/api/auth/login" >login</Button>    
+
+    </div>
+      <div>
+      <Carousel>
+                  <div>
+                      <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172224/1.png" alt="image1"/>
+  
+                  </div>
+                  <div>
+                      <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172225/2.png" alt="image5"/>
+  
+                  </div>
+              </Carousel>
+      </div>
+      <Collapsible trigger="button 1">
+      <p>
+        This is the collapsible content. It can be any element or React
+        component you like.
+      </p>
+    </Collapsible>
+    <Collapsible trigger="button 2">
+      <p>
+        This is the collapsible content. It can be any element or React
+        component you like.
+      </p>
+    </Collapsible>    
       </div>
     );
   }
 
   return (
     <div>
-      <Head>
-        <title>Collaborative Study Portal</title>
-        <meta name="description" content="Learn together Grow together" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
+    <div>
+      <Button href="/api/auth/login" >login</Button>    
+
     </div>
+      <div>
+      <Carousel>
+                  <div>
+                      <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172224/1.png" alt="image1"/>
+  
+                  </div>
+                  <div>
+                      <img src="https://media.geeksforgeeks.org/wp-content/uploads/20211213172225/2.png" alt="image5"/>
+  
+                  </div>
+              </Carousel>
+      </div>
+      <Collapsible  trigger="button 1">
+      <p>
+        This is the collapsible content. It can be any element or React
+        component you like.
+      </p>
+    </Collapsible>
+    <Collapsible trigger="button 2">
+      <p>
+        This is the collapsible content. It can be any element or React
+        component you like.
+      </p>
+    </Collapsible>    
+    </div>
+    
   );
 }
